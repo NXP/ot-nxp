b59ab1d108ab8caaebcb8cef3fe41148ecf23ab3
Alex Porosanu <alexandru.porosanu@nxp.com>
2023-09-06 12:44:13 +0300

    k32w0: examples: hybrid: fix paths due to ZB repo restructuring
    Since the ZB repo now has some files moved from their original placement,
    the hybrid examples (OT+ZB/OT+ZB+BLE) must also accomodate for these
    changes. Also, while here, ensure that the proper zigbee generated files
    are included and not the (empty) ones from the respective examples directory.
    
    Signed-off-by: Alex Porosanu <alexandru.porosanu@nxp.com>

---
+++ examples/hybrid/zb_common/zigbee_gen.h
@@ -0,0 +1,17 @@
+#ifndef __ZIGBEE_GEN_H__
+#define __ZIGBEE_GEN_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef __ASSEMBLER__
+#include "pdum_gen.h"
+#include "zps_gen.h"
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __ZIGBEE_GEN_H__ */
