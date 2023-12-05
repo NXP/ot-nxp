c8a2efa410b74eda47c222809b20a312a02e4204
Cristian Bulacu <cristian.bulacu@nxp.com>
2023-12-05 15:27:20 +0200

    [rw612][boards] Add freertos configuration for BR project.
    This commit aims to add specific freertos configuration files for border
    router project. Non-br configuration is now stored in
    'FreeRTOSConfigCommon.h' and common flags are guarded with #ifndef
    statements.
    If Border router configuration will be modified, #ifnded guard should be added to common file.
    FreeRTOSConfig.h first includes the BR config file, if BR is enabled,
    then the common configuration file.
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

---
+++ boards/rdrw612bga/freertos/FreeRTOSConfigBr.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2023 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef FREERTOS_CONFIG_BR_H
+#define FREERTOS_CONFIG_BR_H
+
+#define configUSE_TICKLESS_IDLE 0
+#define configTOTAL_HEAP_SIZE ((size_t)(159 * 1024))
+
+#endif /* FREERTOS_CONFIG_BR_H */
