f5bec137a03b63de5f3a8b75ab351c582772b50d
Cristian Bulacu <cristian.bulacu@nxp.com>
2023-12-12 10:31:46 +0200

    [rw612] Fix FreeRTOS Config configuration for BR and non-BR
    Added a new folder under 'boards/rdrw612bga/freertos' to accomodate
    specific BR configuration.
    Updated the build script:
    	-setting 'FREERTOS_CONFIG_FILE_DIRECTORY' to
    	point to BR config file when border router target is in use.
    	-made use of already defined 'OT_SRCDIR' variable, defined a new
    	variable 'OT_BR_RTOS_CONFIG_PATH'.
    Setting FREERTOS_CONFIG_FILE_DIRECTORY from build script and not from
    global CMake file will ensure that for the moment, other platform that
    have BR target [RT1170, RT1060] will not be affected, as there is no
    'freertos/br' included for them.
    
    Signed-off-by: Cristian Bulacu <cristian.bulacu@nxp.com>

--- boards/rdrw612bga/freertos/FreeRTOSConfig.h
+++ boards/rdrw612bga/freertos/FreeRTOSConfig.h
@@ -5,12 +5,16 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/**
+ * @file
+ *   This file includes definitions for ot-cli FreeRTOS settings.
+ *   By default, FreeRTOSConfigCommon.h header file will have flags set for the ot-cli application.
+ *   This file is used by default when FREERTOS_CONFIG_FILE_DIRECTORY will not be set to point to other location, i.e
+ *   /boards/rdrw612bga/freertos/br.
+ */
+
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
-
-#if OT_APP_BR_FREERTOS
-#include "FreeRTOSConfigBr.h"
-#endif
 
 #include "FreeRTOSConfigCommon.h"
 
