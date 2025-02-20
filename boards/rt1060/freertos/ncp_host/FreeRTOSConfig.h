1f944288a61a8d7af18bf82cb309ae9214e284c8
Allen Dong <nxf87480@lsv051144.swis.nl-cdc01.nxp.com>
2025-02-20 14:00:16 +0100

    [RT1060][NCP_HOST] Support the function of establishing a secure channel
    Signed-off-by: Allen Dong <allen.dong@nxp.com>

--- boards/rt1060/freertos/ncp_host/FreeRTOSConfig.h
+++ boards/rt1060/freertos/ncp_host/FreeRTOSConfig.h
@@ -55,7 +55,7 @@ extern uint32_t SystemCoreClock;
 #define configTICK_RATE_HZ ((TickType_t)1000)
 #define configMAX_PRIORITIES (10)
 #define configMINIMAL_STACK_SIZE ((uint16_t)1024)
-#define configTOTAL_HEAP_SIZE ((size_t)(60 * 1024))
+#define configTOTAL_HEAP_SIZE ((size_t)(100 * 1024))
 #define configAPPLICATION_ALLOCATED_HEAP 1
 #define configSUPPORT_STATIC_ALLOCATION 0
 #define configSUPPORT_DYNAMIC_ALLOCATION 1
