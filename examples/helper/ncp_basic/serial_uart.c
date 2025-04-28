f32056b02aa280121b0e81fa06562856ec64bb18
George Stefan <george.stefan@nxp.com>
2025-04-28 18:21:00 +0300

    mcxw72_nbu: add NCP basic app for main core
    Provides services to NBU core:
    - non-volatile storage (save/load settings)
    - IEEE 802.15.4 addr
    - serial proxy
    - random seed
    - system reset
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>

---
+++ examples/helper/ncp_basic/serial_uart.c
@@ -0,0 +1,140 @@
+/*
+ *  Copyright (c) 2025, The OpenThread Authors.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. Neither the name of the copyright holder nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "EmbeddedTypes.h"
+
+#include "fsl_component_serial_manager.h"
+#include "fsl_os_abstraction.h"
+#include "ncp_serial_intf.h"
+#include "pin_mux.h"
+
+#define SERIAL_MANAGER_RING_BUFFER_SIZE 2048
+
+static SERIAL_MANAGER_HANDLE_DEFINE(serial_handle);
+static SERIAL_MANAGER_WRITE_HANDLE_DEFINE(serial_read_handle);
+static SERIAL_MANAGER_WRITE_HANDLE_DEFINE(serial_write_handle);
+
+static bool_t serial_init     = FALSE;
+static bool_t rx_data_pending = FALSE;
+
+static uint8_t rx_buff[SERIAL_MANAGER_RING_BUFFER_SIZE];
+static uint8_t ring_buff[SERIAL_MANAGER_RING_BUFFER_SIZE];
+
+static serial_port_uart_config_t uart_config = {.instance     = 1,
+                                                .baudRate     = 115200,
+                                                .parityMode   = kSerialManager_UartParityDisabled,
+                                                .stopBitCount = kSerialManager_UartOneStopBit,
+                                                .enableRx     = 1,
+                                                .enableTx     = 1};
+
+static const serial_manager_config_t config = {
+    .type           = kSerialPort_Uart,
+    .ringBuffer     = ring_buff,
+    .ringBufferSize = SERIAL_MANAGER_RING_BUFFER_SIZE,
+    .portConfig     = &uart_config,
+};
+
+static void rx_cb(void *pData, serial_manager_callback_message_t *message, serial_manager_status_t status)
+{
+    OSA_InterruptDisable();
+    if (!rx_data_pending)
+    {
+        rx_data_pending = TRUE;
+        // PWR_DisallowDeviceToSleep();
+    }
+    OSA_InterruptEnable();
+}
+
+void serial_uart_init()
+{
+    if (serial_init)
+    {
+        return;
+    }
+
+    /* set clock */
+    CLOCK_SetIpSrc(kCLOCK_Lpuart1, kCLOCK_IpSrcFro192M);
+
+    /* enable clock */
+    CLOCK_EnableClock(kCLOCK_Lpuart1);
+
+    uart_config.clockRate = CLOCK_GetIpFreq(kCLOCK_Lpuart1);
+
+    BOARD_InitPinLPUART1_TX();
+    BOARD_InitPinLPUART1_RX();
+
+    /* Init Serial Manager */
+    SerialManager_Init(serial_handle, &config);
+
+    SerialManager_OpenReadHandle(serial_handle, serial_read_handle);
+    SerialManager_OpenWriteHandle(serial_handle, serial_write_handle);
+
+    SerialManager_InstallRxCallback(serial_read_handle, rx_cb, NULL);
+
+    serial_init = TRUE;
+}
+
+void serial_uart_process()
+{
+    uint32_t bytes_read = 0U;
+    bool_t   read_data  = FALSE;
+
+    if (!serial_init)
+    {
+        return;
+    }
+
+    OSA_InterruptDisable();
+    if (rx_data_pending)
+    {
+        read_data       = TRUE;
+        rx_data_pending = FALSE;
+        // PWR_AllowDeviceToSleep();
+    }
+    OSA_InterruptEnable();
+
+    if (read_data)
+    {
+        if ((SerialManager_TryRead(serial_read_handle, rx_buff, sizeof(rx_buff), &bytes_read) ==
+             kStatus_SerialManager_Success) &&
+            (bytes_read != 0))
+        {
+            serial_rpmsg_tx(rx_buff, bytes_read);
+        }
+    }
+}
+
+void serial_uart_tx(uint8_t *data, uint32_t len)
+{
+    if (!serial_init || !data || !len)
+    {
+        return;
+    }
+
+    SerialManager_WriteBlocking(serial_write_handle, data, len);
+}
