d0c7b3ecfb5ffee43bd0ca438fa634b405b611c4
Jamel <jamel.arbi@nxp.com>
2026-02-27 09:00:28 +0100

    mcxw72_nbu: Fix unintended flag removal introduced in commit ca7ddc7acba9
    Signed-off-by: Jamel <jamel.arbi@nxp.com>

--- src/mcxw/mcxw72_nbu/openthread-core-mcxw72_nbu-config.h
+++ src/mcxw/mcxw72_nbu/openthread-core-mcxw72_nbu-config.h
@@ -486,4 +486,9 @@
 #define OPENTHREAD_CONFIG_NET_DIAG_VENDOR_APP_URL "www.nxp.com"
 #endif
 
+/* Increase default timeout (30s) because commissioner doesn't have crypto acceleration */
+#ifndef OPENTHREAD_CONFIG_COMMISSIONER_JOINER_SESSION_TIMEOUT
+#define OPENTHREAD_CONFIG_COMMISSIONER_JOINER_SESSION_TIMEOUT 180
+#endif
+
 #endif // OPENTHREAD_CORE_MCXW72_NBU_CONFIG_H_
